a6a7d33136cf7cc87089ffd678bac719adbcc2c8
Matt <frostmoth@gmail.com>
2021-09-10 09:16:49 -0500

    updated Portfolio 9-10

---
+++ src/components/experience.vue
@@ -0,0 +1,70 @@
+<template>
+    <b-container class="paragraphBox" id="expBox">
+    <b-row>
+      <b-col>
+        <h3 class="test">experience</h3>
+      </b-col>
+    </b-row>
+    <b-row class="test">
+        <b-col sm="12" md="3" lg="3">
+           <a href="https://longhoferperuadventure.com/">
+            <b-img src="images/pla.png" fluid alt="Longhofer Peru Adventure"/>
+           </a>
+        </b-col>
+        <b-col sm="12" md="9" lg="9">
+           <a href="https://longhoferperuadventure.com/"><h3>Longhofer Peru Adventure</h3></a>
+           <h4>June 2019 - July 2019</h4>
+          <p>Produced a brochure website for an adventure edutainment trip to the Amazon. Uses an Accuweather API call to get local temperatures and conditions to put some of the Peruvian destinations on display.  Built with Vue.JS, used the Material theme for the floating action button,  and platformed on Netlify. </p>
+        </b-col>
+    </b-row>
+    <b-row class="test">
+        <b-col sm="12" md="3" lg="3">
+          <a href="https://www.streetsideauto.com/">
+            <b-img src="images/ssa.png" fluid alt="Streetside Auto"/>
+          </a>
+        </b-col>
+        <b-col  sm="12" md="9" lg="9">
+            <a href="https://www.StreetsideAuto.com"><h3>StreetsideAuto.com</h3></a>
+            <h4>Kansas City KS — Front End Web Developer</h4> 
+            <h4>September 2016 - November 2018</h4>
+            <p>Created markup and CSS for the new <a href="https://www.warn.com/">Warn.com</a> that launched July 2018 after 6 months of development. Creation of the markup to make the StreetsideAuto.com website reactive and mobile ready using HTML5, CSS3, jQuery and Bootstrap, managing and updating nopCommerce powered eCommerce websites, created an Angular.JS-powered form to aid in updating product detail pages.</p>
+        </b-col>
+    </b-row>
+    <b-row class="test">
+        <b-col sm="12" md="3" lg="3">
+          <b-img src="images/comp.png" fluid alt="logo"/>
+        </b-col>
+        <b-col  sm="12" md="9" lg="9">
+            <h3>Independent Production Artist</h3>
+            <h4>June 2010 - September  2016</h4>
+            <p>Independent logo design (From sketches to Vector images), Independent development in the Unreal engine for the PC and Mac,  created designs for 3D printing.</p>
+        </b-col>
+    </b-row>
+  </b-container>
+</template>
+<script>
+</script>
+<style scoped>
+
+.test{
+    margin-bottom: 1em;
+    text-align: left;
+}
+#exp3d{
+    margin:auto;
+}
+h3{
+  color: white;
+  
+}
+a{
+text-decoration: none;
+}
+
+@media (min-width: 768px) { 
+  
+  
+}
+
+
+</style>
